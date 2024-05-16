588e791648c372ebf15c5e97c9b26d5becddac57
SRshohan <srshohan02@gmail.com>
2024-05-16 01:16:54 -0400

    Added 2 more functions to store data and Convering content in AIMessage to Str

--- summerizer.py
+++ summerizer.py
@@ -28,16 +28,39 @@ def youtube_transcript(url):
     load = loader.load()
     doc = load[0]
     transcript = doc.page_content
-    with open("storing_transcript.txt", "w") as file:
-        file.write(transcript)
+    
+    return transcript
 
 
 def delete_data():
-    open("storing_transcript.txt", "w").close()
+    f = open("storing_transcript.txt", "w")
+    f.close()
+
+def reading_doc():
+    f = open("storing_transcript.txt", "r")
+    return f.read()
+
+def storing_data(llm_AI_message):
+    llm_AI_message = str(llm_AI_message)
+    f = open("storing_transcript.txt", "w")
+
+    content_start = llm_AI_message.find("content=' ") + len("content=' ")
+    content_end = llm_AI_message.find("' response_metadata")
+    content = llm_AI_message[content_start:content_end]
+
+    return f.write(content)
+
 
 
 if __name__ == '__main__':
+    questions = input("Enter what you want to know about the video: ")
+    transcript = youtube_transcript('https://www.youtube.com/watch?v=0IAPZzGSbME&list=PLDN4rrl48XKpZkf03iYFl-O29szjTrs_O')
+    reading_data = reading_doc()
+    user_input = f"{questions} {reading_data}"
+    llm = load_llm(user_input=user_input)
+    storing_data(llm)
+    print(llm)
     # content = load_llm().invoke(" Write me a peom")
     # print(content.content)
-    youtube_transcript('https://www.youtube.com/watch?v=0IAPZzGSbME&list=PLDN4rrl48XKpZkf03iYFl-O29szjTrs_O')
-    delete_data()
+    
+    

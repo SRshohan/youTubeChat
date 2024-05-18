2e143d02cc52a258c9c3d650cc4df193a16a75c6
SRshohan <srshohan02@gmail.com>
2024-05-18 15:41:07 -0400

    Added a new file to store  user data and 2 separate function to store data

--- backend.py
+++ backend.py
@@ -40,28 +40,41 @@ def reading_doc():
     f = open("storing_transcript.txt", "r")
     return f.read()
 
-def storing_data(llm_AI_message):
+def storing_data_for_transcript(llm_AI_message):
     llm_AI_message = str(llm_AI_message)
-    f = open("storing_transcript.txt", "w")
+    with open(f"storing_transcript.txt", "w") as f:
 
+        """--Converting AImessage to string format """
+        content_start = llm_AI_message.find("content=' ") + len("content=' ")
+        content_end = llm_AI_message.find("' response_metadata")
+        content = llm_AI_message[content_start:content_end]
+        f.write(content + "\n")
 
-    """--Converting AImessage to string format """
-    content_start = llm_AI_message.find("content=' ") + len("content=' ")
-    content_end = llm_AI_message.find("' response_metadata")
-    content = llm_AI_message[content_start:content_end]
+        return content
 
-    return f.write(content)
+def storing_data_of_user(llm_AI_message):
+    llm_AI_message = str(llm_AI_message)
+    with open(f"storing_user_data.txt", "a") as f:
 
+        """--Converting AImessage to string format """
+        content_start = llm_AI_message.find("content=' ") + len("content=' ")
+        content_end = llm_AI_message.find("' response_metadata")
+        content = llm_AI_message[content_start:content_end]
+        f.write(content + "\n")
+
+        return content
 
 
 if __name__ == '__main__':
-    questions = input("Enter what you want to know about the video: ")
+    # questions = input("Enter what you want to know about the video: ")
     transcript = youtube_transcript('https://www.youtube.com/watch?v=0IAPZzGSbME&list=PLDN4rrl48XKpZkf03iYFl-O29szjTrs_O')
+    storing_data_for_transcript(transcript)
     reading_data = reading_doc()
-    user_input = f"{questions} {reading_data}"
+    user_input = f"Tell me in one sentence  {reading_data}"
     llm = load_llm(user_input=user_input)
-    storing_data(llm)
-    print(llm)
+    print(storing_data_of_user(llm))
+    # delete_data()
+    
    
     
     
